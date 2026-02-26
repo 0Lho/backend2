0844d88ebc0f863e8ca3d90f30a133bc8c822d17
0Lho <d3829259@gmail.com>
2026-02-26 19:47:01 +0300

    Update order.js

--- vercel-shop-backend/api/order.js
+++ vercel-shop-backend/api/order.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  // Разрешаем запросы с любого источника
+  // Устанавливаем CORS-заголовки для всех ответов
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
